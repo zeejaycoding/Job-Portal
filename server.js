f685e7fde349461b690145af9d203171773bb71d
Zahab Jahangir <167731882+zeejaycoding@users.noreply.github.com>
2025-05-14 03:08:54 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express=require('express')
 const mongo=require('./mongoConnection');
-const dotenv= require('dotenv').config()
+const dotenv= require('dotenv').config();
+const path = require('path');
 const userController=require('./controller/userController');
+const employeeController=require('./controller/employeeController');
+const candidateController=require('./controller/candidateController');
+const adminController=require('./controller/adminController');
 
 mongo();
 app=express();
@@ -9,6 +13,11 @@ app=express();
 app.use(express.json());
 
 app.use('/',userController);
+app.use('/employee',employeeController);
+app.use('/candidate',candidateController);
+app.use('/admin',adminController);
+app.use('/resumes', express.static(path.join(__dirname, 'resumes')));
+
 
 port=process.env.PORT || 8000
 
